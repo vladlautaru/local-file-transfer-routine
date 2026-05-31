22b3c67a47f4c3c09e937f6969f14366694cef33
vladlautaru <lautaruvlad07@gmail.com>
2026-05-31 19:32:17 +0300

    refactor: implement port, improve console prints

--- src/main/java/io/vladlautaru/client/ClientMain.java
+++ src/main/java/io/vladlautaru/client/ClientMain.java
@@ -27,18 +27,34 @@ public class ClientMain {
             return;
         }
 
-        System.out.println("This is the client program");
+        int port;
+        try {
+            port = ArgsProvider.getPort(argsMap);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
 
-        try (Socket socket = new Socket(host, 2000)){
-            System.out.println("Client socket running at " +
-                    socket.getLocalAddress() +
-                    " on port " +
-                    socket.getLocalPort()
-            );
+        try (Socket socket = new Socket(host, port)){
+            String confirmConnectionMessage =
+                    String.format(
+                            "Server at %s:%d found. Client started on port %d",
+                            host,
+                            port,
+                            socket.getLocalPort()
+                    );
+
+            System.out.println(confirmConnectionMessage);
         } catch (UnknownHostException e) {
             System.out.println("error: unknown host " + host);
         } catch (IOException e) {
-            System.out.println("error: client socket: " + e.getMessage());
+            String error = String.format(
+                    "error: couldn't connect to server at %s:%d",
+                    host,
+                    port
+            );
+
+            System.out.println(error);
         } catch (IllegalArgumentException e) {
             System.out.println("error: invalid port");
         }
