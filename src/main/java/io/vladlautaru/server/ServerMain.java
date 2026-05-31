6e7a48187f28070b136da5166a900f09ffb012f4
vladlautaru <lautaruvlad07@gmail.com>
2026-05-31 14:16:02 +0300

    feat: add basic client-server connection

--- src/main/java/io/vladlautaru/server/ServerMain.java
+++ src/main/java/io/vladlautaru/server/ServerMain.java
@@ -1,7 +1,25 @@
 package io.vladlautaru.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerMain {
     public static void main(String[] args) {
         System.out.println("This is the server program");
+
+        try (ServerSocket serverSocket = new ServerSocket(2000)) {
+            System.out.println("Awaiting connection...");
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Connected to client socket at " +
+                    clientSocket.getInetAddress() +
+                    " on port " +
+                    clientSocket.getLocalPort()
+            );
+        } catch (IOException e) {
+            System.out.println("error: server socket: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("error: invalid port");
+        }
     }
 }
