ad9800571cb94eccd807cb432a61fb6d5acbcb9f
vladlautaru <lautaruvlad07@gmail.com>
2026-05-31 09:45:54 +0300

    initial commit

---
+++ src/main/java/io/vladlautaru/client/ClientMain.java
@@ -0,0 +1,7 @@
+package io.vladlautaru.client;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        System.out.println("This is the client program");
+    }
+}
---
+++ src/main/java/io/vladlautaru/server/ServerMain.java
@@ -0,0 +1,7 @@
+package io.vladlautaru.server;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        System.out.println("This is the server program");
+    }
+}
